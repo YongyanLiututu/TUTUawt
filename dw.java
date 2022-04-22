54208d90c4ee3cd4ccd8da4abe771a055d401743
TuTu <101234558+YongyanLiututu@users.noreply.github.com>
2022-04-22 20:05:57 +0800

    Add files via upload

---
+++ dw.java
@@ -0,0 +1,10 @@
+public class dw {
+    public static void main(String[] args) {
+        boolean a=true;
+
+        boolean b=true;
+    if(a||b){
+        System.out.println("123");
+    }
+    }
+}
